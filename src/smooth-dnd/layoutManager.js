0e8cc2cb7a90b6185a5710cdd6de37ef66697f13
Kutlu Sahin <ksahin@sdl.com>
2018-03-06 16:43:43 +0100

    fix

--- src/smooth-dnd/layoutManager.js
+++ src/smooth-dnd/layoutManager.js
@@ -238,6 +238,7 @@ export default function layoutManager(containerElement, orientation, _animationD
     //getDistanceToContainerBegining,
     getContainerRectangles,
     getBeginEndOfContainer,
+    getBeginEndOfContainerVisibleRect,
     getBeginEnd,
     getAxisValue,
     setTranslation,
