dc486c454fdf4232cf59946de95a1741e018e08c
ahmet kutlu sahin <kutlugsahin@gmail.com>
2018-03-07 22:52:45 +0100

    typo

--- src/demo/pages/horizontal.js
+++ src/demo/pages/horizontal.js
@@ -41,7 +41,7 @@ class Horizontal extends Component {
             }
           </Container>
         <div style={groupStyle}>
-          <Container groupName="1" orientation="horizontal" getChildPayload={i => this.state.items2[i]} onDrop={e => this.setState({ items2: applyDrag(this.state.items2, e) })}>>
+          <Container groupName="1" orientation="horizontal" getChildPayload={i => this.state.items2[i]} onDrop={e => this.setState({ items2: applyDrag(this.state.items2, e) })}>
             {
               this.state.items2.map(p => {
                 return (
