8c644365e8055adcc0083f0064cb6903184c8734
Kutlu Sahin <ksahin@sdl.com>
2018-03-23 11:58:49 +0100

    eslint fix

--- src/Nested.js
+++ src/Nested.js
@@ -2,37 +2,37 @@ import React, { Component } from 'react';
 import { Container, Draggable } from './react-smooth-dnd';
 
 const itemCls = {
-  height: '50px',
-  border: '1px solid #ccc',
-  margin: '5px',
-  textAlign: 'center'
-}
+	height: '50px',
+	border: '1px solid #ccc',
+	margin: '5px',
+	textAlign: 'center'
+};
 
 class Nested extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <Container groupName="1">
-            <div style={itemCls}>Draggable</div>
-            <div style={itemCls}>Draggable 1</div>
-            <div style={itemCls}>Draggable 2</div>
-            <div style={itemCls}>Draggable 3</div>
-            <div style={itemCls}>Draggable 4</div>
-            <div style={{padding: '10px', backgroundColor: "#bca"}}>
-              <Container groupName="1">
-                <div style={itemCls}>Draggable 1</div>
-                <div style={itemCls}>Draggable 1 1</div>
-                <div style={itemCls}>Draggable 1 2</div>
-                <div style={itemCls}>Draggable 1 3</div>
-                <div style={itemCls}>Draggable 1 4</div>
-              </Container>
-            </div>
-          </Container>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div>
+					<Container groupName="1">
+						<div style={itemCls}>Draggable</div>
+						<div style={itemCls}>Draggable 1</div>
+						<div style={itemCls}>Draggable 2</div>
+						<div style={itemCls}>Draggable 3</div>
+						<div style={itemCls}>Draggable 4</div>
+						<div style={{padding: '10px', backgroundColor: '#bca'}}>
+							<Container groupName="1">
+								<div style={itemCls}>Draggable 1</div>
+								<div style={itemCls}>Draggable 1 1</div>
+								<div style={itemCls}>Draggable 1 2</div>
+								<div style={itemCls}>Draggable 1 3</div>
+								<div style={itemCls}>Draggable 1 4</div>
+							</Container>
+						</div>
+					</Container>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Nested;
