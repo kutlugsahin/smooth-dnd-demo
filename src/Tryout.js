93f395d5142c4eb9a16020109463f204d52cea4d
ahmet kutlu sahin <kutlugsahin@gmail.com>
2018-03-24 15:15:16 +0100

    resolve upper level directories

--- src/Tryout.js
+++ src/Tryout.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 export default class extends Component {
-  render() {
-    return (
-      <div id="a">
-        <div id="b" style={{
-          width: '500px',
-          height: '1000px',
-          float: 'left',
-          backgroundColor: 'yellow'
-        }}>
-        </div>
-      </div>
-    );  
-  }
+	render() {
+		return (
+			<div id="a">
+				<div id="b" style={{
+					width: '500px',
+					height: '1000px',
+					float: 'left',
+					backgroundColor: 'yellow'
+				}}>
+				</div>
+			</div>
+		);
+	}
 }
