4f9dc5d463bce77933f4c86435c6f36f1467e996
ahmet kutlu sahin <kutlugsahin@gmail.com>
2018-03-25 22:55:15 +0200

    serviceworker removed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import App from './App';
 import Vanilla from './Vanilla';
 import Nested from './Nested';
 import Tryout from './Tryout';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
