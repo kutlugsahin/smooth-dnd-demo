480aa0e6455e4fc7268b3c473c0667d94c0c45b5
ahmet kutlu sahin <kutlugsahin@gmail.com>
2018-03-26 22:23:12 +0200

    card demo

--- src/demo/pages/cards.js
+++ src/demo/pages/cards.js
@@ -3,12 +3,20 @@ import { Container, Draggable } from 'react-smooth-dnd';
 import { applyDrag } from './utils';
 
 function generateItems(count, creator) {
-	return Array(count).fill().map(creator);
+	return Array(count).fill().map((_, i) => creator(i));
 }
 
 const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+const columnNames = ['Lorem', 'Ipsum', 'Consectetur', 'Eiusmod'];
+
+const cardColors = ['azure', 'beige', 'bisque', 'blanchedalmond', 'burlywood', 'cornsilk', 'gainsboro', 'ghostwhite', 'ivory', 'khaki']
+const pickColor = () => {
+	let rand = Math.floor((Math.random() * 10));
+	return cardColors[rand];
+};
 
 class Cards extends Component {
 	constructor() {
@@ -16,24 +24,27 @@ class Cards extends Component {
 
 		this.onColumnDrop = this.onColumnDrop.bind(this);
 		this.onCardDrop = this.onCardDrop.bind(this);
+		this.getCardPayload = this.getCardPayload.bind(this);
 		this.state = {
 			scene: {
 				type: 'container',
 				props: {
 					orientation: 'horizontal'
 				},
-				children: generateItems(4, (_, i) => ({
+				children: generateItems(4, (i) => ({
 					id: `column${i}`,
 					type: 'container',
+					name: columnNames[i],
 					props: {
 						orientation: 'vertical',
 						className: 'card-container'
 					},
-					children: generateItems(+(Math.random() * 10).toFixed() + 5, (_, j) => ({
+					children: generateItems(+(Math.random() * 10).toFixed() + 5, (j) => ({
 						type: 'draggable',
 						id: `${i}${j}`,
 						props: {
-							className: 'card'
+							className: 'card',
+							style: {backgroundColor: pickColor()}
 						},
 						data: lorem.slice(0, Math.floor(Math.random() * 150) + 30)
 					}))
@@ -46,15 +57,21 @@ class Cards extends Component {
 	render() {
 		return (
 			<div className="card-scene">
-				<Container orientation="horizontal" onDrop={this.onColumnDrop}>
-					{this.state.scene.children.map((column, columnIndex) => {
+				<Container orientation="horizontal" onDrop={this.onColumnDrop} dragHandleSelector=".column-drag-handle">
+					{this.state.scene.children.map((column) => {
 						return (
 							<Draggable key={column.id}>
 								<div className={column.props.className}>
-									<div>ToDo</div>	
+									<div className="card-column-header">
+										<span className="column-drag-handle">&#x2630;</span>
+										{column.name}
+									</div>	
 									<Container {...column.props} groupName="col"
 										onDrop={e => this.onCardDrop(column.id, e)}
-										getChildPayload={index => this.state.scene.children[columnIndex].children[index]}>
+										getChildPayload={index => this.getCardPayload(column.id, index)}
+										dragClass="card-ghost"
+										dropClass="card-ghost-drop"
+									>
 										{column.children.map(card => {
 											return (
 												<Draggable key={card.id}>
@@ -76,6 +93,9 @@ class Cards extends Component {
 		);
 	}
 
+	getCardPayload(columnId, index) {
+		return this.state.scene.children.find(p => p.id === columnId).children[index];
+	}
 
 	onColumnDrop(dropResult) {
 		const scene = Object.assign({}, this.state.scene);
