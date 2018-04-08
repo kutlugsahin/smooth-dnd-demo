02cb0b731b497355cc8560fdd2b3b3a351083153
ahmet kutlu sahin <kutlugsahin@gmail.com>
2018-04-08 15:52:35 +0200

    typo fix

--- src/demo/pages/utils.js
+++ src/demo/pages/utils.js
@@ -1,7 +1,7 @@
 
 export const applyDrag = (arr, dragResult) => {
   const { removedIndex, addedIndex, payload } = dragResult;
-  if (removedIndex === null && addedIndex === null) arr;
+  if (removedIndex === null && addedIndex === null) return arr;
 
   const result = [...arr];
   let itemToAdd = payload;
