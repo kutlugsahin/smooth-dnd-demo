a8d07fad474cdcc0b1adf8a95dfd9d1d82d9402b
Kutlu Sahin <ksahin@sdl.com>
2019-03-11 17:21:13 +0100

    moved to cra2 tsx

---
+++ externals.js
@@ -0,0 +1,10 @@
+module.exports = {
+	'smooth-dnd': {
+		modulePath: '../smooth-dnd',
+		entry: '../smooth-dnd/index.ts'
+	},
+	'react-smooth-dnd': {
+		modulePath: '../react-smooth-dnd',
+		entry: '../react-smooth-dnd/index.ts'
+	}
+}
