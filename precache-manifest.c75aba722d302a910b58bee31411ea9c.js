8605ecda3e49a501b274ddff78f6c3156b6a5d94
ahmet kutlu sahin <kutlugsahin@gmail.com>
2019-03-23 19:09:30 +0100

    Updates

---
+++ precache-manifest.c75aba722d302a910b58bee31411ea9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dbd1f1c86754481cbbd8",
+    "url": "/static/js/main.8fe29bc0.chunk.js"
+  },
+  {
+    "revision": "9e3edc1199af882d5520",
+    "url": "/static/js/2.89796b74.chunk.js"
+  },
+  {
+    "revision": "dbd1f1c86754481cbbd8",
+    "url": "/static/css/main.c834be23.chunk.css"
+  },
+  {
+    "revision": "a68234007e258c7f6e48ecd2fedddfa7",
+    "url": "/index.html"
+  }
+];
